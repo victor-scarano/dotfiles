67ddf02d1bed4e7e25d2bc6c6099fcbbb3012f62
victor-scarano <157558458+victor-scarano@users.noreply.github.com>
2024-06-13 04:26:44 +0000

    Add files via upload

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+vim.keymap.set("n", "<left>", "<nop>")
+vim.keymap.set("n", "<right>", "<nop>")
+vim.keymap.set("n", "<up>", "<nop>")
+vim.keymap.set("n", "<down>", "<nop>")
+
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.showmode = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 4
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 4
+vim.opt.wrap = false
+vim.opt.termguicolors = true
